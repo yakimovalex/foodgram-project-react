7fed8f9b6172bfd7aa650bb199fa8b0e013a1705
Aleksei Iakimov <yakimovizh@gmail.com>
2023-06-07 16:19:51 +0400

    2306071619

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import (Favorite, Follow, Ingredient, IngredientRecipe, Recipe,
+from users.models import Follow
+from .models import (Favorite, Ingredient, IngredientRecipe, Recipe,
                      ShoppingCart, Tag)
 
 
